6b641a030bd48a211dd45b66adfb7f26b2846120
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2023-01-12 11:13:52 -0300

    feat: added typeorm config in customers module

--- src/customers/customers.module.ts
+++ src/customers/customers.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { CustomersService } from './customers.service';
 import { CustomersController } from './customers.controller';
+import { Customer } from '../database/entities/customer.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Customer])],
   controllers: [CustomersController],
   providers: [CustomersService],
 })
