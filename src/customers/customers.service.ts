c8c735e7841d776c10852274e0ff3747a695b6ee
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2023-01-12 11:44:09 -0300

    feat: added function to create and get customer by cpf

--- src/customers/customers.service.ts
+++ src/customers/customers.service.ts
@@ -1,18 +1,90 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  HttpException,
+  HttpStatus,
+  Inject,
+  Injectable,
+  LoggerService,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { Repository } from 'typeorm';
 
+import { Customer } from '../database/entities/customer.entity';
 import { CreateCustomerDto } from './dto/create-customer.dto';
 
 @Injectable()
 export class CustomersService {
-  create(createCustomerDto: CreateCustomerDto) {
-    return 'This action adds a new customer';
+  constructor(
+    @Inject(WINSTON_MODULE_NEST_PROVIDER)
+    private readonly logger: LoggerService,
+
+    @InjectRepository(Customer)
+    private readonly customersRepository: Repository<Customer>,
+  ) {}
+  async create(createCustomerDto: CreateCustomerDto) {
+    try {
+      const { cpf } = createCustomerDto;
+      const customerExists = await this.findOneWithCpf(cpf);
+
+      if (customerExists) {
+        throw new BadRequestException(
+          `Customer with this cpf:${cpf} already exists.`,
+        );
+      }
+
+      const newCustomer = this.customersRepository.create(createCustomerDto);
+      await this.customersRepository.save(newCustomer);
+
+      return await this.findOneWithCpf(newCustomer.cpf);
+    } catch (error) {
+      this.logger.error(error);
+
+      throw new HttpException(
+        error.message,
+        error.status || HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
   findAll() {
     return `This action returns all customers`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} customer`;
+  async findOne(cpf: string) {
+    try {
+      const customer = await this.findOneWithCpf(cpf);
+
+      if (!customer) {
+        throw new NotFoundException(`Customer with this cpf:${cpf} not found.`);
+      }
+
+      return customer;
+    } catch (error) {
+      this.logger.error(error);
+
+      throw new HttpException(
+        error.message,
+        error.status || HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async findOneWithCpf(cpf: string) {
+    try {
+      const customer = await this.customersRepository.findOne({
+        where: { cpf },
+      });
+
+      return customer;
+    } catch (error) {
+      this.logger.error(error);
+
+      throw new HttpException(
+        error.message,
+        error.status || HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
