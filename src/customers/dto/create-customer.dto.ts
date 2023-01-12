2bf74d848f7317554d3108b62b422ff1c0d1b213
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2023-01-12 11:14:06 -0300

    feat: added create customer dto

--- src/customers/dto/create-customer.dto.ts
+++ src/customers/dto/create-customer.dto.ts
@@ -1,1 +1,17 @@
-export class CreateCustomerDto {}
+import { IsString, Length, IsDate, IsNotEmpty } from 'class-validator';
+
+export class CreateCustomerDto {
+  @IsString()
+  @IsNotEmpty()
+  @Length(3, 50)
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(11, 14)
+  cpf: string;
+
+  @IsDate()
+  @IsNotEmpty()
+  birthDate: Date;
+}
