21771e218dd003a74a031e002c12868c56d6485a
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2023-01-12 10:11:40 -0300

    feat: added new env variables and configs for database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,13 @@ import { WinstonModule } from 'nest-winston';
 
 import { LoggerInterceptor } from './logger/logger.interceptor';
 import { winstonAsyncConfigOptions } from './logger/winston.config';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [WinstonModule.forRootAsync(winstonAsyncConfigOptions)],
+  imports: [
+    WinstonModule.forRootAsync(winstonAsyncConfigOptions),
+    DatabaseModule,
+  ],
   providers: [
     {
       provide: APP_INTERCEPTOR,
